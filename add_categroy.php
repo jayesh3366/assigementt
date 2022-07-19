c92ca84f98cf6e2d3f35873750a8ee854f1adf95
jayesh3366 <109573683+jayesh3366@users.noreply.github.com>
2022-07-19 11:14:37 +0530

    Add files via upload

---
+++ add_categroy.php
@@ -0,0 +1,13 @@
+<<form method="post" style="margin-left: 500px; ">
+    <table>
+        <tr> 
+        <td>categroy</td>
+        <td> <input type="text" name="category"></td>
+        </tr>
+
+        <tr>
+            <td colspan="2" align="center"> <input type="submit" name="submit" value="add"></td>
+        </tr>
+    </table>
+    
+</form>
